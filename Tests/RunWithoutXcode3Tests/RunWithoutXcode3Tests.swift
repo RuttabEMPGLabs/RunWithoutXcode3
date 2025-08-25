9abf7b8b6481df693413ac988d5a53030cd77c44
Ruttab <ruttab.haroon@dubizzlelabs.com>
2025-08-25 10:56:01 +0500

    folder struct changed to respect SPM expectation

---
+++ Tests/RunWithoutXcode3Tests/RunWithoutXcode3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  RunWithoutXcode3Tests.swift
+//  RunWithoutXcode3Tests
+//
+//  Created by LPT-Ruttab on 25/08/2025.
+//
+
+import Testing
+@testable import RunWithoutXcode3
+
+struct RunWithoutXcode3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
